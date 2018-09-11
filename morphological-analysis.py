610fcfb0bac1c539d5d720f395ada29aa8bd8381
10380r <10380r@gmail.com>
2018-09-12 01:10:20 +0900

    fixed

--- morphological-analysis.py
+++ morphological-analysis.py
@@ -7,12 +7,9 @@ import sys
 
 url = sys.argv[1]
 
-html = urllib.request.urlopen(url)
+soup = BeautifulSoup(urllib.request.urlopen(url), 'html.parser')
 
-soup = BeautifulSoup(html, 'html.parser')
-
-section = soup.find('section', class_='post-content single-post-content')
-p = section.findAll('p')
+p = soup.findAll('p')
 
 text = ''
 for i in p:
@@ -51,11 +48,10 @@ def create_wordcloud(text):
     fpath = "/Library/Fonts/Ricty-Regular.ttf"
 
     # ノイズが多すぎたので、ストップワードの設定
-    stop_words = ['よう', 'こと', 'の', '方', 'たち', 'ため', 'ー', '>', ':', '<', \
-                  '中', '者', 'やす', '-', 'やすい', 'h', '思う', 'こちら']
+    # stop_words = ['よう', 'こと', 'の', '方', 'たち', 'ため', 'ー', '>', ':', '<', \
+    #               '中', '者', 'やす', '-', 'やすい', 'h', '思う', 'こちら']
 
-    wordcloud = WordCloud(background_color="white", font_path=fpath, width=900, height=500, \
-                          stopwords=set(stop_words)).generate(text)
+    wordcloud = WordCloud(background_color="white", font_path=fpath, width=900, height=500).generate(text)
 
     # 画像の生成
     wordcloud.to_file("results/wordcloud.png")
