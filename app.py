cda17195c149a177936ee2556d44d821c9a93af5
10380r <10380r@gmail.com>
2018-09-12 01:16:19 +0900

    rename

---
+++ app.py
@@ -0,0 +1,61 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import MeCab
+from collections import Counter
+from wordcloud import WordCloud
+import sys
+
+url = sys.argv[1]
+
+soup = BeautifulSoup(urllib.request.urlopen(url), 'html.parser')
+
+p = soup.findAll('p')
+
+text = ''
+for i in p:
+    w = i.text
+    text += w
+
+# 形態素解析
+m = MeCab.Tagger()
+keywords = m.parse(text)
+
+output_words = []
+
+for row in keywords.split('\n'):
+    word = row.split('\t')[0]  # word:単語
+    if word == 'EOS':  # EOS：mecabで終了時出力される
+        break
+    else:
+        pos = row.split('\t')[1].split(',')[0]  #pos: 品詞名
+        if pos == '名詞' or pos == '形容詞':  # 名詞と形容詞のみ抽出
+            output_words.append(word)
+
+counter = Counter(output_words)
+
+wl = []
+
+for i in counter.most_common():
+    wl.append(i)
+
+texts = ''
+for i in wl:
+    texts += i[0] + ' '
+
+
+# wordcloud
+def create_wordcloud(text):
+    fpath = "/Library/Fonts/Ricty-Regular.ttf"
+
+    # ノイズが多すぎたので、ストップワードの設定
+    # stop_words = ['よう', 'こと', 'の', '方', 'たち', 'ため', 'ー', '>', ':', '<', \
+    #               '中', '者', 'やす', '-', 'やすい', 'h', '思う', 'こちら']
+
+    wordcloud = WordCloud(background_color="white", font_path=fpath, width=900, height=500).generate(text)
+
+    # 画像の生成
+    wordcloud.to_file("results/wordcloud.png")
+    print('Done!')
+
+
+create_wordcloud(texts)
